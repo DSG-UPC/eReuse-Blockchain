5519d6d2d35b9443b641e751bdc5d9e9d491d338
emmdim <savemanos@gmail.com>
2019-09-16 16:55:01 +0200

    Includes binary in the abigen conversion

--- abigen.js
+++ abigen.js
@@ -7,9 +7,9 @@ const COMPILED = './build/contracts/'
 const EXPORTED = './go'
 let files = fs.readdirSync(COMPILED);
 
-function abiGen(fil){
-    const name = fil.split(".")[0]
-    const command = `abigen --abi ${path.join(EXPORTED,fil)} --pkg token --type ${name} --out ${path.join(EXPORTED,name+'.go')}`
+function abiGen(abi, bin){
+    const name = abi.split(".")[0]
+    const command = `abigen --abi ${path.join(EXPORTED,abi)} --bin ${path.join(EXPORTED, bin)} --pkg token --type ${name} --out ${path.join(EXPORTED,name+'.go')}`
     exec(command)
     .then( result => {
         if (result.stdout != '') console.log(result.stdout)
@@ -21,10 +21,12 @@ function storeAbi(fil){
     let compiledContract = JSON.parse(fs.readFileSync(path.join(COMPILED,fil)));
     let name_abi = fil.split('.')[0]+'.abi'
     fs.writeFileSync(path.join(EXPORTED,name_abi),JSON.stringify(compiledContract.abi));
-    return name_abi
+    let name_bin = fil.split('.')[0]+'.bin'
+    fs.writeFileSync(path.join(EXPORTED,name_bin),JSON.stringify(compiledContract.bytecode));
+    return [name_abi,name_bin]
 }
 
 if (!fs.existsSync(EXPORTED)){
     fs.mkdirSync(EXPORTED);
 }
-files = files.map(a => {return storeAbi(a)}).map(b => {abiGen(b)})
+files = files.map(a => {return storeAbi(a)}).map(b => {console.log(b,b[0],b[1]);abiGen(b[0],b[1])})
