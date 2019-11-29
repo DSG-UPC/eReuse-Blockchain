ff1dac2635996a9941562d656980a87e22590470
emmdim <savemanos@gmail.com>
2019-11-29 19:46:48 +0100

    Improves Tests by separating them and  adding assert checks

--- test/BasicTest.js
+++ test/BasicTest.js
@@ -6,6 +6,8 @@ const DAO = artifacts.require('DAO');
 const DeviceFactory = artifacts.require('DeviceFactory');
 const DepositDevice = artifacts.require('DepositDevice');
 const DeliveryNote = artifacts.require('DeliveryNote');
+const assert = require('assert');
+const web3 = require('web3')
 
 
 const minimist = require('minimist'),
@@ -14,84 +16,66 @@ const minimist = require('minimist'),
     });
 const network = argv.network;
 
-contract("Basic test with two owners and two device", async function (accounts) {
+contract("Basic test with two owners and two device", function (accounts) {
+    const price = 10;
+    // const initalTokens = 20;
+    var erc20, dao, factory, delivery_note, device_addresses, deviceA, deviceB, devices
+    
+    /// INITIALIZE ACCOUNTS ///
+    const accs = {
+        'ownerA': accounts[1],
+        'ownerB': accounts[2],
+        'ownerC': accounts[3],
+        'ownerD': accounts[4],
+    };
+    console.log('');
+    Object.keys(accs).map(a => {console.log(`${a}: ${accs[a]}`);})
 
-    // it("New device is created and ownerA transfers to ownerB (without delivery note)", async function () {
-
-    //     price = 10;
-
-    //     accs = {
-    //         'ownerA': accounts[1],
-    //         'ownerB': accounts[2]
-    //     };
-
-    //     token = await MyERC721.deployed();
-    //     erc20 = await ERC20.deployed();
-    //     dao = await DAO.deployed();
-    //     factory = await DeviceFactory.deployed();
-
-    //     await erc20.transfer(accs.ownerA, price, { from: accounts[0] });
-    //     await erc20.transfer(accs.ownerB, price, { from: accounts[0] });
-
-    //     /// CREATE THE DEVICE ///
-
-    //     await factory.createDevice("device", 10, accs.ownerA);
-    //     device_address = await factory.getDeployedDevices({ from: accs.ownerA }).then(devices => {
-    //         return devices[0];
-    //     });
-
-    //     await erc20.transfer(device_address, price, { from: accs.ownerA });
-
-    //     console.log('\n## BALANCES INITIALLY ##\n');
-    //     await printBalances(erc20, accs);
-
-    //     /// TRANSFER DEVICE ///
-
-    //     await erc20.transfer(device_address, price, { from: accs.ownerB });
-    //     factory.transfer(device_address, { from: accs.ownerB });
-
-    //     console.log('\n## BALANCES AFTER OWNERS TRANSFER ##\n');
-    //     await printBalances(erc20, accs);
-
-    // });
-
-    it("New device is created and ownerA transfers to ownerB through DeliveryNote)", async function () {
-
-        price = 10;
-
-        accs = {
-            'ownerA': accounts[1],
-            'ownerB': accounts[2]
-        };
-        console.log(`OnwerA: ${accs['ownerA']}`);
-        console.log(`OnwerB: ${accs['ownerB']}`);
-
-
-        token = await MyERC721.deployed();
+    before(async function() {
+        console.log('\t**BEFORE**');
         erc20 = await ERC20.deployed();
         dao = await DAO.deployed();
         factory = await DeviceFactory.deployed();
 
-        await erc20.transfer(accs.ownerA, 2 * price, { from: accounts[0] });
-        await erc20.transfer(accs.ownerB, 2 * price, { from: accounts[0] });
+        await printBalances(erc20,accs)
+        console.log(`\tTransfering Tokens...`);
+        /// TRANSFER TOKENS TO THE PARTICIPANTS
+        for (i in Object.values(accs)) {
+            await erc20.transfer(Object.values(accs)[i], 2 * price, { from: accounts[0] });
+        }
+        await printBalances(erc20,accs)
+    });
 
-
-        /// CREATE THE DEVICE ///
-
+    it("Creates 2 devices and assings them to msg.sender", async function() {
+        // console.log(`\t***Creates 2 devices and assigns them to msg.sender***`);
         await factory.createDevice("deviceA", price, accs.ownerA);
         await factory.createDevice("deviceB", price, accs.ownerA);
         device_addresses = await factory.getDeployedDevices({ from: accs.ownerA }).then(devices => {
             return devices;
         });
-        console.log(`Device addresses ${device_addresses}`);
+        deviceA = await DepositDevice.at(device_addresses[0])
+        deviceB = await DepositDevice.at(device_addresses[1])
+        devices = [deviceA,deviceB]
+        console.log(devices.length);
+        
+        
+        assert.equal(device_addresses.length, devices.length);
+        for (i in devices) {
+            // Check Device onwer is OwnerA
+            assert.equal(accs.ownerA, await devices[i].owner.call())
+            // Check Devices have correct deposit
+            assert.equal(price, await devices[i].getDeposit.call())
+        }
+        await printDeviceOwners(device_addresses);
+    });
 
-
+    it("OwnerA transfers devices to DeliveryNote and emits it", async function () {
+        /// CREATE FIRST DELIVERY NOTE
         delivery_note = await DeliveryNote.new(accs.ownerB, dao.address, { from: accs.ownerA });
-        console.log(`Delivery Note: ${delivery_note.address}`);
+        console.log(`\tDelivery Note: ${delivery_note.address}`);
 
         /// ADDING EACH DEVICE TO THE NEW DELIVERY NOTE ///
-
-        console.log('Owners before adding to delivery note');
+        console.log('\tBefore adding to delivery note');
         await printDeviceOwners(device_addresses);
 
         for (let addr of device_addresses) {
@@ -101,41 +85,56 @@ contract("Basic test with two owners and two device", async function (accounts) 
         }
 
         /// CREATING AND EMITTING THE DELIVERY NOTE ///
-
         await delivery_note.emitDeliveryNote({ from: accs.ownerA });
-        await erc20.approve(delivery_note.address, 2 * price, { from: accs.ownerB });
+        await erc20.approve(delivery_note.address, devices.length * price, { from: accs.ownerB });
+        for (i in devices) {
+            // Check Device onwer is DeliveryNote
+            assert.equal(delivery_note.address, await devices[i].owner.call())
+        }
+    });
 
-        /// ACCEPTING THE DELIVERY NOTE ///
-
-
-        console.log('Before accept delivery note');
+    it("OwnerB accepts to DeliveryNote", async function () {
+        console.log('\tBefore accepting delivery note');
         await printBalances(erc20, accs);
         await printDeviceOwners(device_addresses);
 
         await delivery_note.acceptDeliveryNote({ from: accs.ownerB });
-        devicesB = await factory.getDeployedDevices({ from: accs.ownerB });
-        console.log(devicesB);
+        // devicesB = await factory.getDeployedDevices({ from: accs.ownerB });
 
-        console.log('After accept delivery note');
+        console.log('\tAfter accept delivery note');
 
+        for (i in devices) {
+            // Check Device onwer is DeliveryNote
+            assert.equal(accs.ownerB, await devices[i].owner.call())
+            // Check Device has the promised tokens
+            let deviceTokens = web3.utils.toDecimal(await erc20.balanceOf(devices[i].address))
+            assert.equal(deviceTokens, price)
+        }
+        let ownerBTokens = web3.utils.toDecimal(await erc20.balanceOf(accs.ownerB))
+        console.log(20 - price*devices.length);
+        
+        // Check ownerB does not have the spent tokens
+        assert.equal(ownerBTokens, 20 - price*devices.length)
         await printBalances(erc20, accs);
+        
         await printDeviceOwners(device_addresses);
-
-    });
+    })
 
 });
 async function printBalances(erc20, accounts) {
+    console.log('\n\t BALANCES');
     for (let i in accounts) {
         await erc20.balanceOf(accounts[i]).then(x => {
-            console.log(`${i} balance: ${x}\n`);
+            console.log(`\t${i} balance: ${x}`);
         });
     }
 }
 
 async function printDeviceOwners(devices) {
+    console.log('\n\t OWNERS');
     for (let i in devices) {
         let device = await DepositDevice.at(devices[i]);
         let owner = await device.owner.call();
-        console.log(`Device: ${devices[i]}\tOwner:${owner}`);
+        console.log(`\tDevice: ${devices[i]}\tOwner:${owner}`);
     }
 }
