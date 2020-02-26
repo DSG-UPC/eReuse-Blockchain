9dddf9ee47b3b525cd638fee10c717f6b010df1d
SergioMD15 <sergiomosquera96@gmail.com>
2020-02-26 12:39:55 +0100

    Fix deployments

--- migrations/5_deploy_proofs.js
+++ migrations/5_deploy_proofs.js
@@ -9,31 +9,28 @@ const FunctionProofs = artifacts.require("FunctionProofs");
 
 module.exports = async (deployer, network, accounts) => {
 
-    await DAO.deployed()
-        .then(async function (dao) {
-            await deployer.deploy(ProofsHandler, { from: accounts[0] })
-                .then(async function (handler) {
-                    await deployer.deploy(DataWipeProofs, { from: accounts[0] })
-                        .then(async function (proofs) {
-                            handler.setDataWipeProofs(proofs.address);
-                        });
-                    await deployer.deploy(DisposalProofs, { from: accounts[0] })
-                        .then(async function (proofs) {
-                            handler.setDisposalProofs(proofs.address);
-                        });
-                    await deployer.deploy(ReuseProofs, { from: accounts[0] })
-                        .then(async function (proofs) {
-                            handler.setReuseProofs(proofs.address);
-                        });
-                    await deployer.deploy(RecycleProofs, { from: accounts[0] })
-                        .then(async function (proofs) {
-                            handler.setRecycleProofs(proofs.address);
-                        });
-                    await deployer.deploy(FunctionProofs, { from: accounts[0] })
-                        .then(async function (proofs) {
-                            handler.setFunctionProofs(proofs.address);
-                        });
-                    dao.setProofsHandler(handler.address)
+
+    await deployer.deploy(ProofsHandler, { from: accounts[0] })
+        .then(async function (handler) {
+            await deployer.deploy(DataWipeProofs, { from: accounts[0] })
+                .then(async function (proofs) {
+                    handler.setDataWipeProofs(proofs.address);
+                });
+            await deployer.deploy(DisposalProofs, { from: accounts[0] })
+                .then(async function (proofs) {
+                    handler.setDisposalProofs(proofs.address);
+                });
+            await deployer.deploy(ReuseProofs, { from: accounts[0] })
+                .then(async function (proofs) {
+                    handler.setReuseProofs(proofs.address);
+                });
+            await deployer.deploy(RecycleProofs, { from: accounts[0] })
+                .then(async function (proofs) {
+                    handler.setRecycleProofs(proofs.address);
+                });
+            await deployer.deploy(FunctionProofs, { from: accounts[0] })
+                .then(async function (proofs) {
+                    handler.setFunctionProofs(proofs.address);
                 });
         });
 };
