328448192486b81e4f73226e95e65fc6ee2edec6
emmdim <savemanos@gmail.com>
2020-03-30 20:53:56 +0200

    Adds state and types

--- explorer/lib/types.d.ts
+++ explorer/lib/types.d.ts
@@ -1,36 +1,9 @@
 // NEWS FEED
 
-export interface INewsFeed {
-    // OWN FIELDS
-    version: string,
-    title: string,
-    homePageUrl: string,
-    description: string,
-    feedUrl: string,
-    icon: string,
-    favicon: string,
-    expired: boolean,
-    items: IFeedPost[],
-
-    // INDEXING FIELDS
-    entityId: string,
-    language: string
+let proof : IProof = {
+    proofId: "a"
 }
 
-export interface IFeedPost {
-    id: string,
-    title: string,
-    summary: string,
-    content_text: string,
-    content_html: string,
-    url: string,
-    image: string,
-    tags: string[],
-    date_published: string,
-    date_modified: string,
-
-    author: {
-        name: string,
-        url: string
-    }
+export interface IProof {
+   proofId: number,
 }
